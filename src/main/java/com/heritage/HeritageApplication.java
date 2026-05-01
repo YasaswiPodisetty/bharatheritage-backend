65a2286023d4323b92d496ac8951115f70564cf7
AVUTU DHRUTI <2400030239@kluniversity.in>
2026-05-01 23:44:41 +0530

    Update HeritageApplication.java

--- src/main/java/com/heritage/HeritageApplication.java
+++ src/main/java/com/heritage/HeritageApplication.java
@@ -2,11 +2,13 @@ package com.heritage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.heritage.model")  // 🔥 THIS FIXES YOUR ISSUE
 public class HeritageApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(HeritageApplication.class, args);
     }
 }
-@EntityScan("com.heritage.model")
